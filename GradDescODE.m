0bd9f1b19e4ad92ed05b93f788ad7e85adedafef
FelixxDP <felix.dp.2020@bristol.ac.uk>
2022-07-13 23:08:09 +0100

    Optimising consistency/clarity in code, commented out redundant code

--- GradDescODE.m
+++ GradDescODE.m
@@ -9,17 +9,20 @@
 
 start   = [pi; (3/2)*pi]; % starting moments, column vector length N
 dof     = size(start,1);
-x       = zeros(3,dof); % fix magnet positions: 3xN matrix
-x(1,2)  = 1; % (0,0,0) and (1,0,0) this time
+X       = zeros(3,dof); % fix magnet positions: 3xN matrix
+X(1,2)  = 1; % (0,0,0) and (1,0,0) this time
 
 tauspan = [0 100];
 r0      = start';
-% torque_alpha = @(tau,r) torque(tau,pol2cart(r),x) % change torque input to cart first, so torque
-[tau,r] = ode45(@(tau,r) torque(tau,r,x),tauspan,r0);
+% torque_alpha = @(tau,r) torque(X, pol2cart(r)) % change torque input to cart first, so torque
+[tau,r] = ode45(@(tau,r) torque(X,r),tauspan,r0);
 
 plot(tau,r(:,1),tau,r(:,2))
-xlabel('step (tau)')
-ylabel('\phi')
+ax = gca;
+ax.XLabel.Interpreter = 'latex';
+ax.YLabel.Interpreter = 'latex';
+xlabel('step, $\tau$')
+ylabel('$\phi$')
 legend('\phi_1','\phi_2')
 title('Gradient Descent via ODE method')
 subtitle('x1=(0,0,0), x2=(1,0,0), r0=(pi, (3/2)pi)')
--- torque.m
+++ torque.m
@@ -1,32 +1,35 @@
-function t = torque(tau,dipoles,positions)
-    % calculates the total torque on magnet i from all other magnets
-    % inputs are in that order so it plays nice with ode45
-    
-    % takes positions as 3xn vectors as fixed
-    % takes dipoles as column vector of angles in radians
-    % gives a column vector where ith element is the k component of the
-    % torque acting on ith magnet by all other magnets
-    
-    % currently very half-arsed cos I need this working to test the ode thing
-    % I swear I will return and improve this
-    
-    n   = size(positions,2);
-    Tij = zeros(3,n);
-    Xij = @(Xi, Xj) Xi-Xj;
-    t   = zeros(3,n);
-    m   = hat(dipoles);
-    
-    for i=1:n
-        for a=1:n
-            Tij(:,a) = cross(m(:,i),...
-                             Magnetic_Field(Xij(positions(:,i),positions(:,a)),m(:,a)));
-            if a==i
-                Tij(:,a) = 0;
-            end
-        end
-        t(:,i) = sum(Tij,2);
+function T = torque(positions,dipoles)
+% calculates the total torque on magnet i from all other magnets
+% inputs are in that order so it plays nice with ode45
+
+% takes positions as 3xn vectors as fixed
+% takes dipoles as column vector of angles in radians
+% gives a column vector where ith element is the k component of the
+% torque acting on ith magnet by all other magnets
+
+% currently very half-arsed cos I need this working to test the ode thing
+% I swear I will return and improve this
+
+n   = size(positions,2);
+Tij = zeros(3,n);
+Xij = @(Xi, Xj) Xi-Xj;
+T   = zeros(3,n);
+
+X   = positions;
+M   = hat(dipoles);
+
+for i = 1:n
+    for j = 1:n
+        Tij(:,j) = cross( M(:,i), Magnetic_Field( Xij(X(:,i), X(:,j)), M(:,j)) );
+%         if a==i
+%             Tij(:,a) = 0;
+%         end
     end
-    t(1:2,:) = []; % yes, this is a bodge
-    t = t';
-    
+    T(:,i) = sum(Tij,2,'omitnan');
+end
+
+% extract k components only (assumes 2D rotation)
+T(1:2,:) = []; % yes, this is a bodge
+T = T';
+
 end
